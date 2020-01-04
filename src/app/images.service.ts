f24391da1f976a6f6bdc5ccb5ca12f5fc138cb87
Darin <darindev1000@gmail.com>
2020-01-04 17:33:22 -0500

    Angular or Electron BUG Had to change target to es5 to fix weird bug.

--- src/app/images.service.ts
+++ src/app/images.service.ts
@@ -1,43 +1,27 @@
 import { Injectable } from '@angular/core';
-
 import { BehaviorSubject } from 'rxjs';
 
-const electron = (<any>window).require('electron');
-
+// const electron = (<any>window).require('electron');
+const electron = (window as any).require('electron');
 
 @Injectable({
-
   providedIn: 'root'
-
 })
 
 export class ImagesService {
-
   images = new BehaviorSubject<string[]>([]);
-
   directory = new BehaviorSubject<string[]>([]);
 
-
   constructor() {
-
     electron.ipcRenderer.on('getImagesResponse', (event, images) => {
-
       this.images.next(images);
-
     });
-
     electron.ipcRenderer.on('getDirectoryResponse', (event, directory) => {
-
       this.directory.next(directory);
-
     });
-
   }
 
-
   navigateDirectory(path) {
-
     electron.ipcRenderer.send('navigateDirectory', path);
-
   }
 }
